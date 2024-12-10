537afbe166e127a194e395b37be65022b3ac6a65
ghufron akbar <alan.040219.gam@gmail.com>
2024-12-10 16:13:57 +0700

    leak

--- config/midtransconfig.js
+++ config/midtransconfig.js
@@ -1,9 +1,10 @@
 const midtransClient = require('midtrans-client');
+const { MIDTRANS_SERVER_KEY, MIDTRANS_CLIENT_KEY } = require('../constants');
 
 const midtrans = new midtransClient.Snap({
-    isProduction: false, // Set ke true untuk environment production
-    serverKey: 'SB-Mid-server-f8H8CZX1-QgWp36zWfztt4tQ', // Ganti dengan server key Anda
-    clientKey: 'SB-Mid-client-Uts0BqfcnEk8-8BT', // Ganti dengan client key Anda
+    isProduction: false,
+    serverKey: MIDTRANS_SERVER_KEY,
+    clientKey: MIDTRANS_CLIENT_KEY,
 });
 
 module.exports = midtrans;
